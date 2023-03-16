500cc42016abe29ee79f4fb4f127ba8105dd3d5c
JDCPrograms2017 <jchadwick423@gmail.com>
2023-03-16 00:11:29 -0700

    Modified some authentication features and added a logout button (still not working entirely).

--- app/src/main/java/com/example/basicchatapplication/MainActivity.kt
+++ app/src/main/java/com/example/basicchatapplication/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.basicchatapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var userRecyclerView: RecyclerView
+    private lateinit var userList: ArrayList<User>
+    private lateinit var adapter: UserAdapter
+    private lateinit var mAuth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        userList = ArrayList()
+        adapter = UserAdapter(this, userList)
+        mAuth = FirebaseAuth.getInstance()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu) // We've inflated our menu resource
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.logout) {
+            // logic for the logout process
+            mAuth.signOut()
+            finish()
+            return true
+        }
+        return true
     }
 }
