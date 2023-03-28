8c762611d6da11d00d4e081b1bc97b5474d774e9
Joshua Chadwick <jchadwick423@gmail.com>
2023-03-27 23:46:52 -0700

    Added a comment to ChatActivity.kt

--- app/src/main/java/com/example/nodesocialandroid/ChatActivity.kt
+++ app/src/main/java/com/example/nodesocialandroid/ChatActivity.kt
@@ -3,6 +3,7 @@ package com.example.nodesocialandroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// This will be used when establishing a chatroom between two "friended" users.
 class ChatActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
