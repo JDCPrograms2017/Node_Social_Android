2824b1e153495dfea7da821a2b6020742e4f0fb9
Joshua Chadwick <jchadwick423@gmail.com>
2023-04-01 16:28:52 -0700

    User's profile now updates to current username as stored in the Realtime Database for the current user.

--- app/src/main/java/com/example/nodesocialandroid/UserProfile.kt
+++ app/src/main/java/com/example/nodesocialandroid/UserProfile.kt
@@ -1,13 +1,16 @@
 package com.example.nodesocialandroid
 
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.TextView
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DatabaseReference
-import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
 
 class UserProfile : AppCompatActivity() {
     private lateinit var mAuth: FirebaseAuth
@@ -18,6 +21,26 @@ class UserProfile : AppCompatActivity() {
 
         mAuth = FirebaseAuth.getInstance()
         mDbRef = FirebaseDatabase.getInstance().getReference()
+
+        val txtName = findViewById<TextView>(R.id.username)
+        val currentUserUID = mAuth.currentUser?.uid
+        // Initially updating the user's profile name from the default
+        txtName.text = mDbRef.child("user").child(currentUserUID!!).child("name").get().toString()
+
+        //mDbRef.child(currentUserUID!!).child("name").setValue(null) // UPDATE THIS WHEN YOU MAKE AN EDITABLE TEXT BOX
+
+        // this will update the user's name on their profile from the Realtime Database info if they choose to make a new username
+        mDbRef.child("user").child(currentUserUID!!).addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                txtName.text = snapshot.child("name").value.toString()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w(TAG, "loadPost:onCancelled", error.toException())
+            }
+        })
+
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -31,7 +54,7 @@ class UserProfile : AppCompatActivity() {
             mAuth.signOut()
 
             val intent = Intent(this@UserProfile, Login::class.java)
-            finish()
+            finish() // this will end the current activity
             startActivity(intent) // will bring the user back to the login screen when they opt to log out.
             return true
         }
