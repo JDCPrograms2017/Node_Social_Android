841b56574f9ba96cfa17b6026e54989bded7b5fc
Joshua Chadwick <jchadwick423@gmail.com>
2023-05-25 23:20:16 -0700

    Updated the UserLocatorMap with a getUserLocation function to extract a user's location.

--- app/src/main/java/com/example/nodesocialandroid/UserLocatorMap.kt
+++ app/src/main/java/com/example/nodesocialandroid/UserLocatorMap.kt
@@ -1,7 +1,14 @@
 package com.example.nodesocialandroid
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -9,12 +16,19 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.location.LocationServices
 import com.example.nodesocialandroid.databinding.ActivityMapsBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import java.util.*
 
 class UserLocatorMap : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private lateinit var mFusedLocationClient: FusedLocationProviderClient
+
+    private var userLat: Double = 0.0
+    private var userLong: Double = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,12 +36,15 @@ class UserLocatorMap : AppCompatActivity(), OnMapReadyCallback {
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
-    }
 
+
+    }
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -44,5 +61,41 @@ class UserLocatorMap : AppCompatActivity(), OnMapReadyCallback {
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    private fun getUserLocation() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) { // Ensure we have proper permissions
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                // if we have the permissions, go ahead and get the location and update database
+                if (isLocationEnabled()) {
+                    mFusedLocationClient.lastLocation.addOnCompleteListener(this) {task ->
+                        val userLocation: Location? = task.result
+                        if (userLocation != null) { // If the user's location actually exists...
+                            userLat = userLocation.latitude
+                            userLong = userLocation.longitude
+                        }
+
+                    }
+                }
+            } else {
+                // Request this permission also
+            }
+        } else {
+             // Request the permissions the app needs
+        }
+    }
+
+    // Checks to see if the user has granted us the permissions we wish to request
+    private fun checkPermissions(): Boolean {
+        return ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        var locationPerms: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationPerms.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationPerms.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    private fun updateDatabaseCoords(userLatitude: Double?, userLongitude: Double?) {
+        // This will take in new coords and update the database that we are using with our app to store realtime location information
     }
 }
