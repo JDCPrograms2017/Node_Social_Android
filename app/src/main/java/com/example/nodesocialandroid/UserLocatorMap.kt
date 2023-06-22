f8bab80c90db58c9123b8bd74e0bb9053f4d1edd
Joshua Chadwick <jchadwick423@gmail.com>
2023-06-22 10:30:13 -0700

    Continued work on UserLocatorMap
    Implemented more location-getting functions as demonstrated in https://www.youtube.com/watch?v=vard0CUTLbA and via Android Studio Documentation https://developer.android.com/training/permissions/requesting along with ChatGPT advice.

--- app/src/main/java/com/example/nodesocialandroid/UserLocatorMap.kt
+++ app/src/main/java/com/example/nodesocialandroid/UserLocatorMap.kt
@@ -1,5 +1,6 @@
 package com.example.nodesocialandroid
 
+import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Address
@@ -8,6 +9,7 @@ import android.location.Location
 import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.core.app.ActivityCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -19,16 +21,24 @@ import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.location.LocationServices
 import com.example.nodesocialandroid.databinding.ActivityMapsBinding
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import java.util.*
 
 class UserLocatorMap : AppCompatActivity(), OnMapReadyCallback {
 
+    // TODO: Combine these micro-activities into one Activity class and create a smooth, seamless transition method between user profile, locator map, etc.
+
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
     private lateinit var mFusedLocationClient: FusedLocationProviderClient
+    private lateinit var mAuth: FirebaseAuth
 
     private var userLat: Double = 0.0
     private var userLong: Double = 0.0
+    var PERMISSION_ID = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -64,24 +74,33 @@ class UserLocatorMap : AppCompatActivity(), OnMapReadyCallback {
     }
 
     private fun getUserLocation() {
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) { // Ensure we have proper permissions
-            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                // if we have the permissions, go ahead and get the location and update database
-                if (isLocationEnabled()) {
-                    mFusedLocationClient.lastLocation.addOnCompleteListener(this) {task ->
-                        val userLocation: Location? = task.result
-                        if (userLocation != null) { // If the user's location actually exists...
-                            userLat = userLocation.latitude
-                            userLong = userLocation.longitude
-                        }
+        mAuth = FirebaseAuth.getInstance() // Fetching our current user info. This is repetitive and can be eliminated when we combine all the user profile
+                                           // activities into one Activity class
+        val currentUserID: String? = mAuth.currentUser?.uid
+
+        // if we have the permissions, go ahead and get the location and update database
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
+                mFusedLocationClient.lastLocation.addOnCompleteListener(this) { task ->
+                    val userLocation: Location? = task.result
+                    if (userLocation != null) { // If the user's location actually exists...
+                        userLat = userLocation.latitude
+                        userLong = userLocation.longitude
+
+                        updateDatabaseCoords(userLat, userLong, currentUserID)
+                    }
+                    else { // Get a new location of the user
 
                     }
+
                 }
-            } else {
-                // Request this permission also
             }
-        } else {
-             // Request the permissions the app needs
+            else {
+                // TODO: make notification to tell the user to enable Location Services on their device
+            }
+        }
+        else {
+            requestPermissions()
         }
     }
 
@@ -90,12 +109,40 @@ class UserLocatorMap : AppCompatActivity(), OnMapReadyCallback {
         return ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
     }
 
-    private fun isLocationEnabled(): Boolean {
-        var locationPerms: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        return locationPerms.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationPerms.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(this,
+            arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION), PERMISSION_ID
+        )
     }
 
-    private fun updateDatabaseCoords(userLatitude: Double?, userLongitude: Double?) {
+    // Checks to see if the user's phone has Location Services enabled
+    private fun isLocationEnabled(): Boolean {
+        var locationManager: LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == PERMISSION_ID) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Log.d("Debug:", "The permissions are enabled.")
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    private fun getNewLocation() {
+        //TODO: pick up on the video you were watching, lol
+    }
+
+    // This function will navigate to the child node in the Firebase Realtime Database and update the user's coordinates
+    private fun updateDatabaseCoords(userLatitude: Double?, userLongitude: Double?, userUID: String?) {
         // This will take in new coords and update the database that we are using with our app to store realtime location information
+        val mDatabaseRef = FirebaseDatabase.getInstance().getReference()
+        mDatabaseRef.child("user").child(userUID!!)
     }
 }
